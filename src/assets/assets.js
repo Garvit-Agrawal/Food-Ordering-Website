094b81407677c40ddd0ce7eaa6e127cf5aab4b2c
Garvit Agrawal <garvitagrawal.cse25@jecrc.ac.in>
2024-07-09 11:39:34 +0530

    1st commit

---
+++ src/assets/assets.js
@@ -0,0 +1,357 @@
+import basket_icon from './basket_icon.png'
+import logo from './logo.png'
+import header_img from './header_img.png'
+import search_icon from './search_icon.png'
+import menu_1 from './menu_1.png'
+import menu_2 from './menu_2.png'
+import menu_3 from './menu_3.png'
+import menu_4 from './menu_4.png'
+import menu_5 from './menu_5.png'
+import menu_6 from './menu_6.png'
+import menu_7 from './menu_7.png'
+import menu_8 from './menu_8.png'
+
+import food_1 from './food_1.png'
+import food_2 from './food_2.png'
+import food_3 from './food_3.png'
+import food_4 from './food_4.png'
+import food_5 from './food_5.png'
+import food_6 from './food_6.png'
+import food_7 from './food_7.png'
+import food_8 from './food_8.png'
+import food_9 from './food_9.png'
+import food_10 from './food_10.png'
+import food_11 from './food_11.png'
+import food_12 from './food_12.png'
+import food_13 from './food_13.png'
+import food_14 from './food_14.png'
+import food_15 from './food_15.png'
+import food_16 from './food_16.png'
+import food_17 from './food_17.png'
+import food_18 from './food_18.png'
+import food_19 from './food_19.png'
+import food_20 from './food_20.png'
+import food_21 from './food_21.png'
+import food_22 from './food_22.png'
+import food_23 from './food_23.png'
+import food_24 from './food_24.png'
+import food_25 from './food_25.png'
+import food_26 from './food_26.png'
+import food_27 from './food_27.png'
+import food_28 from './food_28.png'
+import food_29 from './food_29.png'
+import food_30 from './food_30.png'
+import food_31 from './food_31.png'
+import food_32 from './food_32.png'
+
+import add_icon_white from './add_icon_white.png'
+import add_icon_green from './add_icon_green.png'
+import remove_icon_red from './remove_icon_red.png'
+import app_store from './app_store.png'
+import play_store from './play_store.png'
+import linkedin_icon from './linkedin_icon.png'
+import facebook_icon from './facebook_icon.png'
+import twitter_icon from './twitter_icon.png'
+import cross_icon from './cross_icon.png'
+import selector_icon from './selector_icon.png'
+import rating_starts from './rating_starts.png'
+import profile_icon from './profile_icon.png'
+import bag_icon from './bag_icon.png'
+import logout_icon from './logout_icon.png'
+import parcel_icon from './parcel_icon.png'
+import empty_cart from './empty_cart.png'
+import upi from './upi.png'
+import card from './card.png'
+import my_order from './my_order.png'
+export const assets = {
+    logo,
+    basket_icon,
+    header_img,
+    search_icon,
+    rating_starts,
+    add_icon_green,
+    add_icon_white,
+    remove_icon_red,
+    app_store,
+    play_store,
+    linkedin_icon,
+    facebook_icon,
+    twitter_icon,
+    cross_icon,
+    selector_icon,
+    profile_icon,
+    logout_icon,
+    bag_icon,
+    parcel_icon,
+    empty_cart,
+    upi,
+    card,
+    my_order
+}
+
+export const menu_list = [
+    {
+        menu_name: "Salad",
+        menu_image: menu_1
+    },
+    {
+        menu_name: "Rolls",
+        menu_image: menu_2
+    },
+    {
+        menu_name: "Deserts",
+        menu_image: menu_3
+    },
+    {
+        menu_name: "Sandwich",
+        menu_image: menu_4
+    },
+    {
+        menu_name: "Cake",
+        menu_image: menu_5
+    },
+    {
+        menu_name: "Pure Veg",
+        menu_image: menu_6
+    },
+    {
+        menu_name: "Pasta",
+        menu_image: menu_7
+    },
+    {
+        menu_name: "Noodles",
+        menu_image: menu_8
+    }]
+
+export const food_list = [
+    {
+        _id: "1",
+        name: "Greek salad",
+        image: food_1,
+        price: 12,
+        description: "A refreshing and vibrant mix of crisp cucumbers, juicy tomatoes, Kalamata olives, and tangy feta cheese, all tossed in a light and zesty lemon-oregano vinaigrette. Perfect for a light meal or a delightful side dish",
+        category: "Salad"
+    },
+    {
+        _id: "2",
+        name: "Veg salad",
+        image: food_2,
+        price: 18,
+        description: "A colorful medley of fresh, crunchy vegetables including bell peppers, carrots, cucumbers, and cherry tomatoes, drizzled with a light and herby vinaigrette. This healthy and satisfying salad is perfect for any occasion",
+        category: "Salad"
+    }, {
+        _id: "3",
+        name: "Clover Salad",
+        image: food_3,
+        price: 16,
+        description: "A unique blend of tender baby spinach, mixed greens, and arugula, topped with toasted almonds, dried cranberries, and a hint of goat cheese. Finished with a honey-mustard dressing for a touch of sweetness",
+        category: "Salad"
+    }, {
+        _id: "4",
+        name: "Chicken Salad",
+        image: food_4,
+        price: 24,
+        description: "Succulent pieces of grilled chicken breast mixed with crisp celery, red onions, and crunchy almonds, all tossed in a creamy and tangy dressing. Ideal for a nutritious lunch or as a sandwich filling",
+        category: "Salad"
+    }, {
+        _id: "5",
+        name: "Lasagna Rolls",
+        image: food_5,
+        price: 14,
+        description: "Delicious lasagna pasta rolled up with a rich and savory filling of ricotta cheese, spinach, and ground beef, then baked to perfection with a layer of marinara sauce and melted mozzarella. A fun twist on a classic favorite",
+        category: "Rolls"
+    }, {
+        _id: "6",
+        name: "Peri Peri Rolls",
+        image: food_6,
+        price: 12,
+        description: "Spicy and flavorful, these rolls are filled with succulent chicken marinated in a fiery Peri Peri sauce, along with fresh veggies and wrapped in a soft, warm tortilla. A perfect grab-and-go meal with a kick",
+        category: "Rolls"
+    }, {
+        _id: "7",
+        name: "Chicken Rolls",
+        image: food_7,
+        price: 20,
+        description: "Tender and juicy chicken, mixed with fresh veggies and herbs, wrapped in a soft tortilla. These rolls are bursting with flavor and make for a perfect quick and easy meal or snack",
+        category: "Rolls"
+    }, {
+        _id: "8",
+        name: "Veg Rolls",
+        image: food_8,
+        price: 15,
+        description: "A wholesome and delicious mix of fresh vegetables, lightly seasoned and wrapped in a soft tortilla. These rolls are a great vegetarian option, full of crunch and flavor",
+        category: "Rolls"
+    }, {
+        _id: "9",
+        name: "Ripple Ice Cream",
+        image: food_9,
+        price: 14,
+        description: "Creamy and smooth ice cream swirled with luscious ribbons of flavored syrup, creating a delightful combination of taste and texture. Each bite offers a ripple of sweetness that melts in your mouth",
+        category: "Deserts"
+    }, {
+        _id: "10",
+        name: "Fruit Ice Cream",
+        image: food_10,
+        price: 22,
+        description: "A creamy and refreshing dessert made with a blend of seasonal fruits, offering a burst of natural sweetness and a delightful way to cool down on a hot day. Perfect for fruit lovers",
+        category: "Deserts"
+    }, {
+        _id: "11",
+        name: "Jar Ice Cream",
+        image: food_11,
+        price: 10,
+        description: "Indulge in layers of creamy ice cream, rich sauces, and delectable toppings all packed into a convenient and charming jar. A perfect treat for satisfying your sweet tooth, anytime and anywhere",
+        category: "Deserts"
+    }, {
+        _id: "12",
+        name: "Vanilla Ice Cream",
+        image: food_12,
+        price: 12,
+        description: "Classic and timeless, this rich and creamy vanilla ice cream is made with real vanilla beans for an authentic flavor. Perfect on its own or as a complement to your favorite desserts",
+        category: "Deserts"
+    },
+    {
+        _id: "13",
+        name: "Chicken Sandwich",
+        image: food_13,
+        price: 12,
+        description: "A hearty and satisfying sandwich featuring tender, grilled chicken breast, crisp lettuce, juicy tomatoes, and a touch of mayo, all nestled between two slices of toasted bread. Ideal for a quick lunch or dinner",
+        category: "Sandwich"
+    },
+    {
+        _id: "14",
+        name: "Vegan Sandwich",
+        image: food_14,
+        price: 18,
+        description: "A delicious and nutritious sandwich loaded with fresh vegetables, avocado, hummus, and a mix of flavorful spices, all packed between slices of whole grain bread. Perfect for a healthy and filling vegan meal",
+        category: "Sandwich"
+    }, {
+        _id: "15",
+        name: "Grilled Sandwich",
+        image: food_15,
+        price: 16,
+        description: "A warm and crispy sandwich with a gooey melted cheese center, combined with your choice of fillings such as ham, turkey, or vegetables. Grilled to golden perfection for a comforting and delicious meal",
+        category: "Sandwich"
+    }, {
+        _id: "16",
+        name: "Bread Sandwich",
+        image: food_16,
+        price: 24,
+        description: "Simple yet satisfying, this classic sandwich features fresh, soft bread filled with your choice of spreads and fillings, such as peanut butter and jelly, egg salad, or tuna salad. A versatile option for any time of the day",
+        category: "Sandwich"
+    }, {
+        _id: "17",
+        name: "Cup Cake",
+        image: food_17,
+        price: 14,
+        description: "Delightfully moist and fluffy cupcakes topped with a generous swirl of creamy frosting and a sprinkle of colorful decorations. These little cakes are perfect for any celebration or just a sweet treat",
+        category: "Cake"
+    }, {
+        _id: "18",
+        name: "Vegan Cake",
+        image: food_18,
+        price: 12,
+        description: "A delicious and indulgent cake made entirely without animal products. Moist and flavorful, this cake is perfect for vegans and non-vegans alike, offering a guilt-free way to enjoy dessert",
+        category: "Cake"
+    }, {
+        _id: "19",
+        name: "Butterscotch Cake",
+        image: food_19,
+        price: 20,
+        description: "Rich and decadent, this cake features layers of moist sponge cake infused with the buttery, caramel-like flavor of butterscotch, and topped with a creamy butterscotch frosting. A treat for any occasion",
+        category: "Cake"
+    }, {
+        _id: "20",
+        name: "Sliced Cake",
+        image: food_20,
+        price: 15,
+        description: "Conveniently pre-sliced for easy serving, this cake comes in a variety of flavors and is perfect for parties, gatherings, or simply satisfying your sweet tooth. Each slice is a piece of deliciousness waiting to be enjoyed",
+        category: "Cake"
+    }, {
+        _id: "21",
+        name: "Garlic Mushroom ",
+        image: food_21,
+        price: 14,
+        description: "Sautéed mushrooms infused with the rich and savory flavors of garlic and herbs, creating a deliciously aromatic and earthy dish. Perfect as an appetizer or a side dish to complement any meal",
+        category: "Pure Veg"
+    }, {
+        _id: "22",
+        name: "Fried Cauliflower",
+        image: food_22,
+        price: 22,
+        description: "Crispy and golden fried cauliflower florets, lightly seasoned and served with a tangy dipping sauce. A delightful and crunchy snack or appetizer that's sure to please",
+        category: "Pure Veg"
+    }, {
+        _id: "23",
+        name: "Mix Veg Pulao",
+        image: food_23,
+        price: 10,
+        description: "A fragrant and flavorful rice dish cooked with a medley of fresh vegetables, aromatic spices, and herbs. This vibrant and nutritious pulao is perfect as a main course or a side dish",
+        category: "Pure Veg"
+    }, {
+        _id: "24",
+        name: "Rice Zucchini",
+        image: food_24,
+        price: 12,
+        description: "A light and healthy dish featuring fluffy rice mixed with tender zucchini, garlic, and herbs. Perfect for a nutritious meal that's both delicious and satisfying",
+        category: "Pure Veg"
+    },
+    {
+        _id: "25",
+        name: "Cheese Pasta",
+        image: food_25,
+        price: 12,
+        description: "Creamy and indulgent, this pasta is smothered in a rich cheese sauce made from a blend of cheeses, creating a velvety and comforting dish that's perfect for cheese lovers",
+        category: "Pasta"
+    },
+    {
+        _id: "26",
+        name: "Tomato Pasta",
+        image: food_26,
+        price: 18,
+        description: "A classic Italian dish with al dente pasta tossed in a vibrant and tangy tomato sauce, made from ripe tomatoes, garlic, and fresh basil. Simple yet delicious, this pasta is a timeless favorite",
+        category: "Pasta"
+    }, {
+        _id: "27",
+        name: "Creamy Pasta",
+        image: food_27,
+        price: 16,
+        description: "Luxurious and silky pasta coated in a rich and creamy sauce, often enhanced with garlic, herbs, and a touch of Parmesan. Perfect for a comforting and satisfying meal",
+        category: "Pasta"
+    }, {
+        _id: "28",
+        name: "Chicken Pasta",
+        image: food_28,
+        price: 24,
+        description: "Tender pieces of chicken combined with perfectly cooked pasta and tossed in a savory sauce, often creamy or tomato-based. A hearty and flavorful dish that's perfect for dinner",
+        category: "Pasta"
+    }, {
+        _id: "29",
+        name: "Buttter Noodles",
+        image: food_29,
+        price: 14,
+        description: "Simple yet delicious, these noodles are tossed in melted butter and lightly seasoned with salt and pepper. A comforting and easy dish that's perfect for a quick meal",
+        category: "Noodles"
+    }, {
+        _id: "30",
+        name: "Veg Noodles",
+        image: food_30,
+        price: 12,
+        description: "A colorful and tasty dish featuring stir-fried noodles mixed with a variety of fresh vegetables and seasoned with soy sauce and spices. A wholesome and flavorful option for noodle lovers",
+        category: "Noodles"
+    }, {
+        _id: "31",
+        name: "Somen Noodles",
+        image: food_31,
+        price: 20,
+        description: "Thin and delicate Japanese noodles served either cold with a dipping sauce or hot in a savory broth. These noodles are light, refreshing, and perfect for any time of the year",
+        category: "Noodles"
+    }, {
+        _id: "32",
+        name: "Cooked Noodles",
+        image: food_32,
+        price: 15,
+        description: "Versatile and convenient, these noodles are pre-cooked and ready to be tossed with your favorite sauce, vegetables, or proteins. A quick and easy solution for a delicious meal",
+        category: "Noodles"
+    }
+]
